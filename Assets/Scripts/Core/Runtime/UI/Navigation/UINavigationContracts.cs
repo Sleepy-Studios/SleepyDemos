4ae36f79d8ad7eb6432f7df2da2c0ab232b3892f
黄柏维 <huangbowei@galasports.com>
2026-07-12 00:43:20 +0800

    fix(ui): 收紧导航结果契约

--- Assets/Scripts/Core/Runtime/UI/Navigation/UINavigationContracts.cs
+++ Assets/Scripts/Core/Runtime/UI/Navigation/UINavigationContracts.cs
@@ -21,10 +21,10 @@ namespace Core.Runtime
 
     public enum UIOperationStatus
     {
-        Succeeded,
-        Canceled,
-        Failed,
-        Ignored
+        Succeeded = 1,
+        Canceled = 2,
+        Failed = 3,
+        Ignored = 4
     }
 
     public enum UITransitionDirection
@@ -88,9 +88,15 @@ namespace Core.Runtime
         /// <param name="action">导航操作类型。</param>
         /// <param name="view">导航操作关联的 View。</param>
         /// <returns>成功状态的导航操作结果。</returns>
+        /// <exception cref="ArgumentNullException"><paramref name="view"/> 为 null。</exception>
         public static UIOperationResult Succeeded(long id, UINavigationAction action, View view)
         {
-            return new UIOperationResult(id, action, UIOperationStatus.Succeeded, view, null);
+            return new UIOperationResult(
+                id,
+                action,
+                UIOperationStatus.Succeeded,
+                view ?? throw new ArgumentNullException(nameof(view)),
+                null);
         }
 
         /// <summary>
@@ -100,9 +106,15 @@ namespace Core.Runtime
         /// <param name="action">导航操作类型。</param>
         /// <param name="view">导航操作关联的 View。</param>
         /// <returns>忽略状态的导航操作结果。</returns>
+        /// <exception cref="ArgumentNullException"><paramref name="view"/> 为 null。</exception>
         public static UIOperationResult Ignored(long id, UINavigationAction action, View view)
         {
-            return new UIOperationResult(id, action, UIOperationStatus.Ignored, view, null);
+            return new UIOperationResult(
+                id,
+                action,
+                UIOperationStatus.Ignored,
+                view ?? throw new ArgumentNullException(nameof(view)),
+                null);
         }
 
         /// <summary>
@@ -125,13 +137,19 @@ namespace Core.Runtime
         /// <param name="view">导航操作关联的 View。</param>
         /// <param name="exception">导航操作失败时的异常。</param>
         /// <returns>失败状态的导航操作结果。</returns>
+        /// <exception cref="ArgumentNullException"><paramref name="exception"/> 为 null。</exception>
         public static UIOperationResult Failed(
             long id,
             UINavigationAction action,
             View view,
             Exception exception)
         {
-            return new UIOperationResult(id, action, UIOperationStatus.Failed, view, exception);
+            return new UIOperationResult(
+                id,
+                action,
+                UIOperationStatus.Failed,
+                view,
+                exception ?? throw new ArgumentNullException(nameof(exception)));
         }
     }
 
--- Assets/Scripts/Tests/Core/EditMode/UINavigationContractsTests.cs
+++ Assets/Scripts/Tests/Core/EditMode/UINavigationContractsTests.cs
@@ -1,3 +1,4 @@
+using System;
 using Core.Runtime;
 using NUnit.Framework;
 
@@ -5,6 +6,7 @@ namespace Core.Tests.UI
 {
     public sealed class UINavigationContractsTests
     {
+        [TestCase(UILayer.Underground, UIViewMode.Page)]
         [TestCase(UILayer.Base, UIViewMode.Page)]
         [TestCase(UILayer.Foreground, UIViewMode.Page)]
         [TestCase(UILayer.Pop, UIViewMode.Modal)]
@@ -27,6 +29,94 @@ namespace Core.Tests.UI
             Assert.That(result.Action, Is.EqualTo(UINavigationAction.Push));
             Assert.That(result.View, Is.SameAs(view));
             Assert.That(result.Exception, Is.Null);
+        }
+
+        [Test]
+        public void DefaultResult_IsNotSucceededAndContainsNoPayload()
+        {
+            var result = default(UIOperationResult);
+
+            Assert.That(result.Status, Is.Not.EqualTo(UIOperationStatus.Succeeded));
+            Assert.That(result.View, Is.Null);
+            Assert.That(result.Exception, Is.Null);
+        }
+
+        [Test]
+        public void DefaultShowOptions_EnablesAnimation()
+        {
+            Assert.That(default(UIShowOptions).Animated, Is.True);
+        }
+
+        [Test]
+        public void ExplicitFalseShowOptions_DisablesAnimation()
+        {
+            Assert.That(new UIShowOptions(false).Animated, Is.False);
+        }
+
+        [Test]
+        public void Succeeded_ThrowsWhenViewIsNull()
+        {
+            Assert.Throws<ArgumentNullException>(
+                () => UIOperationResult.Succeeded(1, UINavigationAction.Push, null));
+        }
+
+        [Test]
+        public void Ignored_ThrowsWhenViewIsNull()
+        {
+            Assert.Throws<ArgumentNullException>(
+                () => UIOperationResult.Ignored(2, UINavigationAction.Preload, null));
+        }
+
+        [Test]
+        public void Failed_ThrowsWhenExceptionIsNull()
+        {
+            Assert.Throws<ArgumentNullException>(
+                () => UIOperationResult.Failed(3, UINavigationAction.Replace, new View(), null));
+        }
+
+        [Test]
+        public void IgnoredResult_ContainsOperationPayloadAndNoException()
+        {
+            var view = new View();
+
+            var result = UIOperationResult.Ignored(4, UINavigationAction.Preload, view);
+
+            Assert.That(result.Status, Is.EqualTo(UIOperationStatus.Ignored));
+            Assert.That(result.OperationId, Is.EqualTo(4));
+            Assert.That(result.Action, Is.EqualTo(UINavigationAction.Preload));
+            Assert.That(result.View, Is.SameAs(view));
+            Assert.That(result.Exception, Is.Null);
+        }
+
+        [Test]
+        public void CanceledResult_AllowsNullViewAndContainsNoException()
+        {
+            var result = UIOperationResult.Canceled(5, UINavigationAction.Close, null);
+
+            Assert.That(result.Status, Is.EqualTo(UIOperationStatus.Canceled));
+            Assert.That(result.OperationId, Is.EqualTo(5));
+            Assert.That(result.Action, Is.EqualTo(UINavigationAction.Close));
+            Assert.That(result.View, Is.Null);
+            Assert.That(result.Exception, Is.Null);
+        }
+
+        [Test]
+        public void FailedResult_ContainsOperationPayloadAndException()
+        {
+            var view = new View();
+            var exception = new InvalidOperationException("failed");
+
+            var result = UIOperationResult.Failed(
+                6,
+                UINavigationAction.Replace,
+                view,
+                exception);
+
+            Assert.That(result.Status, Is.EqualTo(UIOperationStatus.Failed));
+            Assert.That(result.OperationId, Is.EqualTo(6));
+            Assert.That(result.Action, Is.EqualTo(UINavigationAction.Replace));
+            Assert.That(result.View, Is.SameAs(view));
+            Assert.That(result.Exception, Is.SameAs(exception));
         }
     }
 }
